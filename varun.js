b212b0d309f55bc0133a623923b1939040e818ae
varunraj3011 <1596.sfm@gmail.com>
2025-03-30 16:02:53 +0530

    Your commit message

--- varun.js
+++ varun.js
@@ -1,10 +1,11 @@
 var show=document.getElementById("show")
 var side=document.getElementById("side")
 show.addEventListener("click",function(){
-    side.style.left="0"
+    side.style.right="0"
 
 })
 var close=document.getElementById("xmark")
 close.addEventListener("click",function(){
-    side.style.left="-60%"
+    side.style.right="-60%"
 })
+
